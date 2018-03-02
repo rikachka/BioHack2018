85ec496ecac5f444c91a68db0c19a261442e9d22
izeren <yushalnik@bk.ru>
2018-03-02 23:56:41 +0300

    Add clever argparse with help

--- run_dir.py
+++ run_dir.py
@@ -1,8 +1,39 @@
 import sys
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description='hmmer genomes')
+
+parser.add_argument(
+	'--source_dir', 
+	help='dir with genomes. all .faa files will be processed (default: genomes)', 
+	default='genomes')
+
+parser.add_argument(
+	'--db_dir', 
+	help='dir with db files for hmmscan processing (default: hmmDB)',
+	default='hmmDB')
+
+parser.add_argument(
+	'--result_dir', 
+	help='dir where will be saved .rep files (default: reps)',
+	default='reps')
+
+def soft_mkdir(dirname):
+    try:
+        os.mkdir(dirname)
+    except Exception as e:
+        print('directory has been created before')
+
+
 if __name__ == '__main__':
-	dirname = sys.argv[1]
-	result_dir = sys.argv[2]
+	args = parser.parse_args()
+	dirname = args.source_dir
+	db_dir = args.db_dir
+	result_dir = args.result_dir
+	soft_mkdir(result_dir)
 	files = sorted(os.listdir(dirname))
 	for filename in files:
-		os.system('hmmscan --tblout {0}/{1} ./hmmDB/bdb {2}/{1}'.format(result_dir, filename, dirname))
+		command = 'hmmscan --tblout {0}/{2}.rep {1}/bdb {3}/{2}'.format(result_dir, db_dir, filename, dirname)
+		print(command)
+		os.system(command)
