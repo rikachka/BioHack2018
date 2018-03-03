29f1676a92687bdfc0da83f33d1165a0afcd4274
izeren <yushalnik@bk.ru>
2018-03-03 20:31:29 +0300

    Add get_frequency

--- utils.py
+++ utils.py
@@ -10,3 +10,23 @@ def parse_enzymes(file_path):
             enzymes[enzyme_id].append(line)
     f.close()
     return enzymes
+
+
+# unique_sequences = [set(sequence) for sequence in sequences]
+# sequences = [np.load('numpy_dir/{}'.format(filename)) for filename in sorted(os.listdir('numpy_dir/'))]
+def get_frequency(sequences):
+    copyness = {}
+    total = 0
+    for sequence in sequences:
+        for wog in sequence:
+            if wog is not None:
+                total += 1
+                if wog in copyness:
+                    copyness[wog] += 1
+                else:
+                    copyness[wog] = 1
+    for key, value in copyness.items():
+        copyness[key] = value / total
+    return copyness
+
+
